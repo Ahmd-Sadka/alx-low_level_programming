f8a4e109beab329f24ffe9320e1bd03ef33d9598
Ahmd-sadka <flookaty@gmail.com>
2023-07-13 17:08:54 -0700

    print

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio>
+#include<unist.h>
+/**
+ * main - printing new text
+ * Return: 1 (success)
+ */
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
