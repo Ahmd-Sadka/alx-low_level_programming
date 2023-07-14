90bdfa1a9806815795c2ced63f5f6509018af50f
Ahmd-sadka <flookaty@gmail.com>
2023-07-14 13:34:35 -0700

    complex comb

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - print combinations of two two-digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+int i = 0;
+while (i < 100)
+{
+int j = i;
+while (j < 100)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+putchar(' ');
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+j++;
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
