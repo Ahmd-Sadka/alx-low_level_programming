403080cc90edf850dd8996357cdb467a69ab5916
Ahmd-Sadka <flookaty@gmail.com>
2023-09-26 10:02:19 -0700

    mem st

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#!/bin/bash
+#include "main.h"
+/*
+**_memset() - fills memory with const value
+* the first n bytes of the memory area
+* pointed to by s with the constant byte b
+*@s:target
+*@b: constant byte
+*@n:number of byte
+*Return: returns new value of target
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+
+return (s);
+}
+
