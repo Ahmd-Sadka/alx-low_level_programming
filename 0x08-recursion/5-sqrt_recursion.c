57da6b7359f175bb414a76ee1dfb4fc849677d42
Ahmd-Sadka <flookaty@gmail.com>
2023-09-28 07:26:04 -0700

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * sqrt - returns square root
+ * @y: int
+ * @val: initial
+ * Return: int
+ */
+
+int sqrt(int y, int val)
+{
+if (val * val == y)
+	return (y);
+else if (val * val < y)
+	return (sqrt(y, val + 1));
+else
+	return (-1);
+}
+
+/**
+ *_sqrt_recursion - returns square root of number
+ *@n: int
+ *Return: int
+ */
+
+int _sqrt_recursion(int n)
+{
+return (sqrt(n, 1));
+}
