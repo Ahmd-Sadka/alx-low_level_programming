3f33da305a0f5c23501cfce9527a2ea565bc003e
Ahmd-Sadka <flookaty@gmail.com>
2023-10-31 10:33:51 -0700

    concat str

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,32 +15,25 @@ char *str_concat(char *s1, char *s2)
 	char *concat_str;
 	int index, concat_index, size;
 
-	if (s1 == 0)
+	if (s1 == NULL)
 		s1 = "";
-	if (s2 == 0)
+
+	if (s2 == NULL)
 		s2 = "";
-	for (index = 0; s1[index] != "\0"; index++)
-	{
-	}
-	for (concat_index = 0; s1[concat_index] != "\0"; concat_index++)
-	{
-	}
 
-	concat_str = malloc(sizeof(char) * (index + concat_index + 1));
+	for (index = 0; s1[index] || s2[index]; index++)
+		size++;
 
-	if (concat_str == 0)
-		return (0);
+	concat_str = malloc(sizeof(char) * size);
+
+	if (concat_str == NULL)
+		return (NULL);
 
 	for (index = 0; s1[index]; index++)
-	{
-		concat_str[concat_index] = s1[index];
-		concat_index++;
-	}
+		concat_str[concat_index++] = s1[index];
 
-	for (concat_index++ = 0; s2[concat_index++]; concat_index++)
-	{
-		concat_str[concat_index] = s2[index];
-	}
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s2[index];
 
 	return (concat_str);
 }
