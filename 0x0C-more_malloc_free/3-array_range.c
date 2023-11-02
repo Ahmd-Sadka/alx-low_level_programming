a2729be97a46e4c904262003afe94bcaa6a0c778
Ahmd-Sadka <flookaty@gmail.com>
2023-11-02 14:40:20 -0700

    min to max int

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * array_range - function that creates an array of integers
+ * @min: min array value
+ * @max: max array value
+ * Return: pointer to the newly created array
+ */
+
+int *array_range(int min, int max)
+{
+	int *nemo;
+	int i, j = 0;
+
+	if (min > max)
+		return (NULL);
+
+	nemo = malloc(sizeof(nemo) * ((max - main) + 1));
+
+	if (nemo == NULL)
+		return (NULL);
+
+	else
+	{
+		for (i = min; i < max; i++)
+		{
+			nemo[j] = i;
+			j++;
+		}
+		return (nemo);
+	}
+}
