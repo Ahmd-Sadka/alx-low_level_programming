ec69b28fac42dfe797af5a5857b6cc9aa62780ff
Ahmd-Sadka <flookaty@gmail.com>
2023-11-06 07:22:46 -0800

    abs

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef Function_Like_Macro.H
-#define Function_Like_Macro.H
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) (((x) < (0)) ? ((-1) * (x)) : (x))
+#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
 
 #endif
