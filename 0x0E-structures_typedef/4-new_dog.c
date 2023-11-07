379dce140eac56bb2d7d7ff3f1b4b04bf005aac9
Ahmd-Sadka <flookaty@gmail.com>
2023-11-07 02:47:49 -0800

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,28 +1,29 @@
 #include "dog.h"
 
-/**
- * _strlen - func return sring length
- * @str: string input
- * Return: len
- */
 
+/**
+*_strlen - returns length of
+*a string
+*@str: string to be counted
+*Return: returns length of string
+*/
 int _strlen(char *str)
 {
-	int len = 0;
+int len = 0;
+while (str)
+len++;
 
-	while (str)
-		len++;
-
-	return (len);
+return (len);
 }
 
-/**
- * _strcopy - copy string from src to dest
- * @dest: new copy
- * @src: source to copy
- * Return: cpy of string
- */
 
+/**
+*_strcopy - copy string pointed by src
+*into dest variable
+*@dest:buffer storing string copy
+*@src: buffer storing string to copy
+*Return:returns copied string
+*/
 char *_strcopy(char *dest, char *src)
 {
 int index = 0;
@@ -34,14 +35,17 @@ dest[index] = '\0';
 return (dest);
 }
 
-/**
- **new_dog - func creates a new dog
- *@name: new dog name
- *@age: new dog age
- *@owner: new owner
- *Return:  copy of name and owner or Return NULL
- */
 
+
+
+/**
+*new_dog - creates a new dog
+*@name: name of new dog
+*@age: age of new dog
+*@owner: owner of new dog
+*Return: returns NULL in case
+*of failure
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *doggo;
