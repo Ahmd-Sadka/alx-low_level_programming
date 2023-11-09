3bcb8dfd39fe13454d74e23b86117438395219ef
Ahmd-Sadka <flookaty@gmail.com>
2023-11-08 16:36:04 -0800

    nce

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "calc.h"
+
+/**
+ *opp_add - add operation
+ *@a: 1st num
+ *@b: 2nd num
+ *Return: a+b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *opp_sub - subtracion operation
+ *@a: 1st num
+ *@b: 2nd num
+ *Return: a-b
+ */
+
+int op_sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ *opp_mul - multiply operation
+ *@a: 1st num
+ *@b: 2nd num
+ *Return: a*b
+ */
+
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ *opp_div - division operation
+ *@a: 1st num
+ *@b: 2nd num
+ *Return: a/b
+ */
+
+int op_div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ *opp_mod - modulus operation
+ *@a: 1st num
+ *@b: 2nd num
+ *Return: a%b
+ */
+
+int op_mod(int a, int b)
+{
+        return (a % b);
+}
+
