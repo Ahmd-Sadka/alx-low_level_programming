70405c17a50caca5c0e6e3d2917a793c9c8cef2b
Ahmd-Sadka <flookaty@gmail.com>
2023-11-27 12:18:07 -0800

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ *create_file - Create a function that creates a file.
+ *
+ *@filename: file name
+ *@text_content: pointer to string
+ *Return: 1 (succcess) or -1 (error)
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int counter = 0, fp = 0, outp = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	if (fp == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[counter] != '\0')
+			counter++;
+	}
+	else
+	{
+		close(fp);
+		return (1);
+	}
+	outp = write(fp, text_content, counter);
+	if (outp == -1 || outp != counter)
+		return (-1);
+
+	close (fp);
+	return (1);
+
+}
